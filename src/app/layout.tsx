3785845f4e8ceab0818ad4799b9d4a55c963fa28
shap87 <shane87web@gmail.com>
2023-07-28 20:22:58 +0300

    fix: add animation for Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={lato.className}>{children}</body>
+      <body className={lato.className} suppressHydrationWarning={true}>{children}</body>
     </html>
   )
 }
